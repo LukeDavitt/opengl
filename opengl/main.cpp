07cf937b73dc02ba735f5a3d5ccc2362306d8f27
Luke Davitt <lukedavitt@lukes-mac-mini.local>
2015-10-11 09:49:05 -0400

    Initial Commit

---
+++ opengl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl
+//
+//  Created by Luke Davitt on 10/11/15.
+//  Copyright © 2015 Luke Davitt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
